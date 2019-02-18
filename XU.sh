11e6164174f0efd7c670894e5a5b3a48780db1f3
ketaketish <38956824+ketaketish@users.noreply.github.com>
2019-02-18 02:57:51 -0500

    Add files via upload

---
+++ XU.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "############################################"
+echo "# This option is still under development   #"
+echo "# There are currently no options to update #"
+echo "############################################"
+sleep 2
+echo ""
+echo "closing now..."
+sleep 2
+exit
+
+
+
